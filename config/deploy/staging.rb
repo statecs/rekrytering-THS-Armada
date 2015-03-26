668efd22a1e97f2de0c110b0cecd3625a93340f3
statecs <statec@icloud.com>
2015-03-27 00:26:54 +0100

    staging.rb - git sync

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,9 @@
+
+
+set :rails_env, "staging"
+set :branch, "master" #fetch(:branch, "new_layout")
+#server 'stocken.armada.nu', :db, :app, :web, :primary => true
+set :env, fetch(:env, 'staging')
 server 'web.armada.nu', :app, :web, :primary => true
+role :db,  'web.armada.nu', :primary => true
+set :deploy_to, "/var/www/rekrytering.armada.nu"
